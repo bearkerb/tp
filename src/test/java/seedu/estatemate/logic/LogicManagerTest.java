3538479857c2d5ed6b833d35d5bea9824647d965
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-10-09 10:45:41 +0800

    Change comment to refer to AddTenantCommand

--- src/test/java/seedu/estatemate/logic/LogicManagerTest.java
+++ src/test/java/seedu/estatemate/logic/LogicManagerTest.java
@@ -164,7 +164,7 @@ public class LogicManagerTest {
 
         logic = new LogicManager(model, storage);
 
-        // Triggers the saveEstateMate method by executing an add command
+        // Triggers the saveEstateMate method by executing an add tenant command
         String addTenantCommand = AddTenantCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY
                 + EMAIL_DESC_AMY + ADDRESS_DESC_AMY;
         Person expectedPerson = new PersonBuilder(AMY).withTags().build();

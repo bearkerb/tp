8d8888f53eaee3e1368af2f381d4b614b8de1a64
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-10-20 18:11:56 +0800

    Update validity checking for Lease

--- src/main/java/seedu/estatemate/model/person/Lease.java
+++ src/main/java/seedu/estatemate/model/person/Lease.java
@@ -5,6 +5,7 @@ import static seedu.estatemate.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Represents a Person's lease in EstateMate.
@@ -18,7 +19,7 @@ public class Lease {
      * The first character of the lease must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final String VALIDATION_REGEX = "\\d{4}-\\d{2}-\\d{2} \\d{4}-\\d{2}-\\d{2}";
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
@@ -34,14 +35,40 @@ public class Lease {
     public Lease(String lease) {
         requireNonNull(lease);
         checkArgument(isValidLease(lease), MESSAGE_CONSTRAINTS);
+
+        String[] dates = getDates(lease);
+        startDate = LocalDate.parse(dates[0], DATE_FORMATTER);
+        endDate = LocalDate.parse(dates[1], DATE_FORMATTER);
+
         value = lease;
     }
 
     /**
-     * Returns true if a given string is a valid lease.
+     * Returns true if a given string is in a valid lease format.
      */
     public static boolean isValidLease(String test) {
-        return test.matches(VALIDATION_REGEX);
+        boolean isValidFormat = test.matches(VALIDATION_REGEX);
+        boolean isValidDates = true;
+
+        String[] dates = getDates(test);
+        String startDate = dates[0];
+        String endDate = dates[1];
+
+        try {
+            LocalDate.parse(startDate, DATE_FORMATTER);
+            LocalDate.parse(endDate, DATE_FORMATTER);
+        } catch (DateTimeParseException e) {
+            isValidDates = false;
+        }
+        return isValidFormat && isValidDates;
+    }
+
+    /**
+     * Parses the lease for its start and end dates.
+     * @return A string array with the first element as the start date and second element as the end date.
+     */
+    private static String[] getDates(String lease) {
+        return lease.split(" ");
     }
 
     @Override

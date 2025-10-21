5cc2b676735e971e8b14474379f54819d00662a3
bearkerb <gabrieltan7879@gmail.com>
2025-10-21 15:13:19 +0800

    Add missing JavaDoc comment for ListJobCommand class

--- src/main/java/seedu/estatemate/logic/commands/ListJobCommand.java
+++ src/main/java/seedu/estatemate/logic/commands/ListJobCommand.java
@@ -4,7 +4,9 @@ import static java.util.Objects.requireNonNull;
 import static seedu.estatemate.model.Model.PREDICATE_SHOW_ALL_JOBS;
 
 import seedu.estatemate.model.Model;
-
+/**
+ * Lists all jobs to the user.
+ */
 public class ListJobCommand extends Command {
     public static final String COMMAND_WORD = "ljob";
 

9ac139d3ff9bd58cdf73a8546559a4d1a3bfbb72
Benjamin Lim <phantomgubiak@gmail.com>
2025-10-29 10:17:31 +0800

    Fix JavaCI violations

--- src/main/java/seedu/estatemate/model/Model.java
+++ src/main/java/seedu/estatemate/model/Model.java
@@ -6,7 +6,6 @@ import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
 import seedu.estatemate.commons.core.GuiSettings;
-import seedu.estatemate.logic.commands.exceptions.CommandException;
 import seedu.estatemate.model.job.Description;
 import seedu.estatemate.model.job.Job;
 import seedu.estatemate.model.person.Person;

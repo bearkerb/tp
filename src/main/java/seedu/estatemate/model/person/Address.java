9b488046578e95043404cf0fd24ce2e08e6fcdac
Yorick Chew <178005181+yorick-chew@users.noreply.github.com>
2025-10-29 20:12:55 +0800

    Update param classes javadoc to tenant

--- src/main/java/seedu/estatemate/model/person/Address.java
+++ src/main/java/seedu/estatemate/model/person/Address.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.estatemate.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's address in EstateMate.
+ * Represents a tenant's address in EstateMate.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
 public class Address {

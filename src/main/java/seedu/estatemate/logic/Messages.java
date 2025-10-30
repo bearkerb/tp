818c069279ad77f83a5c6b61dec703c2b86c744e
gooZXSean <e1398111@u.nus.edu>
2025-10-30 09:01:08 +0800

    Update and align find, list for tenants and jobs

--- src/main/java/seedu/estatemate/logic/Messages.java
+++ src/main/java/seedu/estatemate/logic/Messages.java
@@ -16,8 +16,8 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Invalid tenant index detected.";
-    public static final String MESSAGE_JOBS_LISTED_OVERVIEW = "%1$d jobs listed!";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_JOBS_LISTED_OVERVIEW = "Listed %1$d jobs(s).";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "Listed %1$d tenant(s).";
     public static final String MESSAGE_DUPLICATE_FIELDS =
             "Multiple values specified for the following single-valued field(s): ";
     public static final String MESSAGE_INVALID_JOB_ID = "The job index provided is invalid";
